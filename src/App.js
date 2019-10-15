8f6d2a7b102db9d5a89ceff5f8097dc92d5bed42
goldrix <goldrix96@gmail.com>
2019-10-15 16:49:01 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="selectLanguage">
+            <h1>Please select your language</h1>
+        </div>
+        <div className="buttonEnglish">
+            <button>English</button>
+        </div>
+        <div className="buttonEnglish">
+            <button>English</button>
+        </div>
+        <div className="buttonEnglish">
+            <button>English</button>
+        </div>
+        <div className="buttonEnglish">
+            <button>English</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
