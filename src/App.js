c80c0efa2de1190ab5a74f56afb6e7ff0946c568
= <=>
2019-10-15 21:15:52 -0400

    hello

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import './App.css';
+import englishButton from './images/english-button.png'
+import spanishButton from './images/spanish.png'
+import chineseButton from './images/chinese.png'
+import russianButton from './images/russian.png'
+
 
 function App() {
   return (
@@ -9,17 +14,19 @@ function App() {
             <h1>Please select your language</h1>
         </div>
         <div className="buttonEnglish">
-            <button>English</button>
+            <img className="buttons" src ={englishButton} alt={"englishButton"}/>
         </div>
         <div className="buttonEnglish">
-            <button>English</button>
+            <img className="buttons" src ={chineseButton} alt={"chineseButton"}/>
         </div>
         <div className="buttonEnglish">
-            <button>English</button>
+            <img className="buttons" src ={spanishButton} alt={"spanishButton"}/>
         </div>
         <div className="buttonEnglish">
-            <button>English</button>
+            <a href="https://medium.com/@goldrix96"><img className="buttons" src ={russianButton} alt={"russianButton"}/></a>
+            
         </div>
+
       </div>
     </div>
   );
